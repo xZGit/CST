2fade90a20279b43a3e778012ad0cdf7b36b8ed0
zhongxia <418760129@qq.com>
2015-06-04 17:45:52 +0800

    add data

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+/**
+ * Created by xx on 15/6/4.
+ */
+
+
+
+(function (module) {
+
+    module.categories = [
+        {id: 1, name: "文艺", description: "", score: 109},
+        {id: 2, name: "二", description: "", score: 110},
+        {id: 3, name: "高逼格", description: "", score: 106},
+        {id: 4, name: "无节操", description: "", score: 109},
+        {id: 5, name: "腐", description: "", score: 109},
+        {id: 6, name: "宅", description: "", score: 101},
+        {id: 7, name: "萌", description: "", score: 103},
+        {id: 8, name: "高智商", description: "", score: 101},
+    ];
+
+
+    module.apps = [
+        {id: 1, name: "豆瓣FM", imageUrl: "", category: [{id: 1, score: 8}]},
+        {id: 2, name: "ONE", imageUrl: "", category: [{id: 1, score: 9}]},
+        {id: 3, name: "特逗", imageUrl: "", category: [{id: 2, score: 9}]},
+        {id: 4, name: "美图gif", imageUrl: "", category: [{id: 2, score: 6}]},
+        {id: 1, name: "MONO", imageUrl: "", category: [{id: 3, score: 9}]},
+        {id: 1, name: "LOFTER", imageUrl: "", category: [{id: 3, score: 7}]},
+        {id: 1, name: "糗事百科", imageUrl: "", category: [{id: 4, score: 8}]},
+        {id: 1, name: "节操精选", imageUrl: "", category: [{id: 4, score: 7}]},
+        {id: 1, name: "陌陌", imageUrl: "", category: [{id: 5, score: 6}]},
+        {id: 1, name: "饿了么", imageUrl: "", category: [{id: 5, score: 8}]},
+        {id: 1, name: "豆腐", imageUrl: "", category: [{id: 6, score: 10}]},
+        {id: 1, name: "有妖气", imageUrl: "", category: [{id: 6, score: 8}]},
+        {id: 1, name: "美图贴贴", imageUrl: "", category: [{id: 7, score: 8}]},
+        {id: 1, name: "颜文字", imageUrl: "", category: [{id: 7, score: 8}]},
+        {id: 1, name: "Framed", imageUrl: "", category: [{id: 8, score: 8}]},
+        {id: 1, name: "The room", imageUrl: "", category: [{id: 8, score: 9}]},
+        {id: 1, name: "INS", imageUrl: "", category: [{id: 1, score: 2}, {id: 3, score: 3}]},
+        {id: 1, name: "美拍", imageUrl: "", category: [{id: 7, score: 2}, {id: 2, score: 2}]},
+        {id: 1, name: "百思不得其姐", imageUrl: "", category: [{id: 5, score: 2}, {id: 4, score: 4}]},
+        {id: 1, name: "开心消消乐", imageUrl: "", category: [{id: 8, score: 2}, {id: 6, score: 4}]},
+    ];
+
+
+}(exports));
