e5fcf0fbdfac4dce31fc252ad76c99585950e9ce
zhongxia <418760129@qq.com>
2015-06-06 15:14:31 +0800

    add wechat support

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,12 +6,11 @@
 "use strict";
 
 var querystring = require('querystring');
-var async = require('async');
-var OAuth = require('wechat-oauth');
+var OAuth = require('../../lib/wechatOauth');
 var config = require('../../conf/config');
 var client = new OAuth(config.app.appId, config.app.appsecret);
 var categories = require('../data/data');
-var categoryNames =["apps", "movies"];
+var categoryNames = ["apps", "movies"];
 
 
 var url = client.getAuthorizeURL(config.app.redirectUrl, config.app.state, config.app.scope);
@@ -28,52 +27,41 @@ Controllers.index = function *() {
 
 var a = 0;
 Controllers.home = function *() {
-    var session = this.session;
-    if (!session.count) session.count = a++;
-    this.body = yield this.render({count: session.count}, "basic");
+
+    var that = this;
+    async.auto({
+        getAccessToken: function (callback) {
+            callback();
+        },
+
+    }, function (err, results) {
+
+        that.body = 77;
+    });
 };
 
 
 Controllers.getCategory = function *() {
     var category = categories[0];
-    if(this.params.category) category = categories[this.params.category] || categories[0];
+    if (this.params.category) category = categories[this.params.category] || categories[0];
     this.body = category;
 };
 
 
-
-
-Controllers.setCategory = function *(){
-
-
+Controllers.setCategory = function *() {
 
 
 };
 
 
-Controllers.getUserInfo = function *(){
-   var param= querystring.parse(this.request.url.split('?')[1]);
-   var that=this;
-    async.auto({
-        getAccessToken: function(callback){
-            client.getAccessToken(param.code, callback);
-        },
-        getUserInfo:["getAccessToken", function(callback, results){
-            client.getUser(results.getAccessToken.data.openid, callback);
-        }]
-    },function(err, results){
+Controllers.getUserInfo = function *() {
+    var param = querystring.parse(this.request.url.split('?')[1]);
+    var accessReq = yield client.getAccessToken("011f8aa7d5b3e17622a10d68cee8352");
+    var user = yield client.getUser(accessReq.data.openid);
 
-        that.session.openid=results.getUserInfo.openid;
-        that.session.nickname=results.getUserInfo.nickname;
 
-    });
-      that.body=that.render({}, "index");
+    this.body = this.render({}, "index");
 };
-
-
-
-
-
 
 
 module.exports = Controllers;
