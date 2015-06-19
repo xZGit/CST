164bf3c84cad6afecce58c77b6f0e977255f05b7
zhongxia <418760129@qq.com>
2015-06-19 12:34:45 +0800

    add views

--- src/routes.js
+++ src/routes.js
@@ -25,41 +25,8 @@ module.exports = function (app) {
 
     app.get("/getUserInfo", controller.getUserInfo);
 
-    app.get("/getUserInfo", controller.getUserInfo);
+    app.get("/back/:category", controller.back);
 
-    app.get("/test-app",function *(){
-            this.body = yield this.render({}, "test-app");
-    }
-   );
-
-
-    app.get("/test-book",function *(){
-            this.body = yield this.render({}, "test-book");
-        }
-    );
-    app.get("/test-music",function *(){
-            this.body = yield this.render({}, "test-music");
-        }
-    );
-
-    app.get("/test-role",function *(){
-            this.body = yield this.render({}, "test-role");
-        }
-    );
-
-    app.get("/test-start",function *(){
-            this.body = yield this.render({}, "test-start");
-        }
-    );
-
-    app.get("/test-video",function *(){
-            this.body = yield this.render({}, "test-video");
-        }
-    );
-    app.get("/test-end",function *(){
-            this.body = yield this.render({}, "test-end");
-        }
-    );
     app.post("/api/setCategory", controller.setCategory);
 
     setUpPageRoute("/getResult/:id", controller.getResult);
